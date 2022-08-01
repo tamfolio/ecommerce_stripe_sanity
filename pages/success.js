785cf83b231c550741c3fed4a2b25119d413f8ad
unknown <tamiloreolaniyi@gmail.com>
2022-08-01 06:32:40 +0100

    success page

---
+++ pages/success.js
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from 'react'
+import Link from 'next/link';
+import {BsBagCheckFill} from 'react-icons/bs';
+import { useRouter } from 'next/router';
+import { useStateContext } from '../context/StateContext';
+
+
+const Success = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Success
