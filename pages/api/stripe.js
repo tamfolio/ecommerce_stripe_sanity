a5e84fff673b05a260ef11f2ebb9e063ae4c89db
unknown <tamiloreolaniyi@gmail.com>
2022-08-01 06:24:41 +0100

    stripe payment

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,33 +1,46 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-
-        console.log(req.body.cartItems)
         try {
-
             const params = {
-                    submit_type: 'pay',
-                    mode: 'payment',
-                    payment_method_types: ['card'],
-                    billing_address_collection: 'auto',
-                    shipping_options: [
-                        { shipping_rate: 'shr_1LQdwHFWWQFcOCYH8Cm56TZI' },
-                        { shipping_rate: 'shr_1LQdzBFWWQFcOCYHRai3Qzsf' },
-                    ],
-                    line_items: req.body.cartItems.map((item) => {
-                        const img = item.image[0].asset._ref;
-                        const newImage = img.replace('image-', 'https://cdn.sanity.io/images/mamn9pvu/production/').replace('-webp', '.webp');
+                submit_type: 'pay',
+                mode: 'payment',
+                payment_method_types: ['card'],
+                billing_address_collection: 'auto',
+                shipping_options: [
+                    { shipping_rate: 'shr_1LQdwHFWWQFcOCYH8Cm56TZI' },
+                ],
+                line_items: req.body.map((item) => {
+                    const img = item.image[0].asset._ref;
+                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
 
-                        console.log('IMAGE', newImage)
-                    }),
-                    mode: 'payment',
-                    success_url: `${req.headers.origin}/?success=true`,
-                    cancel_url: `${req.headers.origin}/?canceled=true`,
-                }
-                // Create Checkout Sessions from body params.
+                    return {
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: item.name,
+                                images: [newImage],
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                        },
+                        quantity: item.quantity
+                    }
+                }),
+                success_url: `${req.headers.origin}/success`,
+                cancel_url: `${req.headers.origin}/canceled`,
+            }
+
+            // Create Checkout Sessions from body params.
             const session = await stripe.checkout.sessions.create(params);
-            res.redirect(303, session.url);
+
+            res.status(200).json(session);
         } catch (err) {
             res.status(err.statusCode || 500).json(err.message);
         }
@@ -36,4 +49,3 @@ export default async function handler(req, res) {
         res.status(405).end('Method Not Allowed');
     }
 }
-sanity.io
